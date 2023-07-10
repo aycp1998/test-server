cb5fddbb8136b413d28629d710c59315ba634dde
Alexander Yosimar Cervantes Pineda <aycp.developer@outlook.com>
2023-07-09 20:55:39 -0600

    model configurado

---
+++ src/main/java/com/test/server/model/Data.java
@@ -0,0 +1,71 @@
+package com.test.server.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "data")
+public class Data {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private Integer age;
+
+    @Column(name = "country")
+    private String country;
+
+    public Data() {
+    }
+
+    public Data(String name, Integer age, String country) {
+        this.name = name;
+        this.age = age;
+        this.country = country;
+    }
+
+    public Data(Long id, String name, Integer age, String country) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.country = country;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+}
