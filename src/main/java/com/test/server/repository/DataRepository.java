6d36f70088cb97c22350435bd98bf15810b01fde
Alexander Yosimar Cervantes Pineda <aycp.developer@outlook.com>
2023-07-09 20:56:10 -0600

    repository configurado

---
+++ src/main/java/com/test/server/repository/DataRepository.java
@@ -0,0 +1,9 @@
+package com.test.server.repository;
+
+import com.test.server.model.Data;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DataRepository extends JpaRepository<Data, Long> {
+}
